f0570ecd8f039712f8a42cb7367f03d6f811c694
Stefan Stanisev <shadowrageinc@gmail.com>
2021-10-27 13:08:05 +0300

    [NET-BGP] Add sidenav bar, basic js function for open/close for sidenav, credit-profile image

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,11 +1,28 @@
-$(document).ready(function() {
-  $(".answer").hide();
+      let sidebar = document.querySelector(".sidebar");
+      let closeBtn = document.querySelector("#btn");
+      let menuTitle = document.querySelector(".logo_name");
+      let profile = document.querySelector(".profile");
+    
+      closeBtn.addEventListener("click", ()=>{
+        sidebar.classList.toggle("open");
+        menuBtnChange(); // will open the sidebar on btn click;
+      });
 
-  $("#btn-show").click(function() {
-    $(".answer").show();
-  });
+      profile.addEventListener("click", ()=>{
+        sidebar.classList.toggle("open");
+        menuBtnChange(); // will open the sidebar on profile info click;
+      });
 
-  $("#btn-hide").click(function() {
-    $(".answer").hide();
-  });
-});
+      menuTitle.addEventListener("click", ()=>{
+        sidebar.classList.toggle("open");
+        menuBtnChange(); // will open the sidebar on Menu title click;
+      });
+    
+      // following are the code to change sidebar button(optional)
+      function menuBtnChange() {
+       if(sidebar.classList.contains("open")){
+         closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+       }else {
+         closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+       }
+      }
