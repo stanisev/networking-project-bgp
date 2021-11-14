b66e491b637876a6be35db85f5325f8062e25a74
Stefan Stanisev <shadowrageinc@gmail.com>
2021-11-14 18:21:27 +0200

    [CONTENT-004] Add questions and answes page

--- src/js/functions.js
+++ src/js/functions.js
@@ -3,23 +3,44 @@
       let closeBtn = document.querySelector("#btn");
       let menuTitle = document.querySelector(".logo_name");
       let profile = document.querySelector(".profile");
+
+      let q_answer = document.querySelectorAll(".q_answer");
+      let answer_button = document.querySelector(".margin-style");
+      let isShown = false;
+
+      answer_button.addEventListener("click", () => {
+        hideShowAnswers();
+       });
+
+       function hideShowAnswers() {
+         if (isShown) {
+           q_answer.forEach(value => value.style.display = 'none');
+           isShown = false;
+           answer_button.innerHTML = 'Покажи отговорите';
+
+         } else {
+          q_answer.forEach(value => value.style.display = 'block');;
+          isShown = true;
+          answer_button.innerHTML = 'Скрий отговорите';
+         }
+       }
     
-      closeBtn.addEventListener("click", ()=>{
-       if(isNotMedia()) {
+      closeBtn.addEventListener("click", () => {
+       if (isNotMedia()) {
         toogleOpen();
         menuBtnChange(); // will open the sidebar on btn click;
        }
       });
 
-      profile.addEventListener("click", ()=>{
-        if(isNotMedia()) {
+      profile.addEventListener("click", () => {
+        if (isNotMedia()) {
           toogleOpen();
           menuBtnChange(); // will open the sidebar on profile info click;
          }
       });
 
-      menuTitle.addEventListener("click", ()=>{
-        if(isNotMedia()) {
+      menuTitle.addEventListener("click", () => {
+        if (isNotMedia()) {
           toogleOpen();
           menuBtnChange(); // will open the sidebar on Menu title click;
          }
@@ -27,10 +48,10 @@
     
       // following are the code to change sidebar button(optional)
       function menuBtnChange() {
-       if(sidebar.classList.contains("open")){
-         closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
-       }else {
-         closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+       if (sidebar.classList.contains("open")) {
+         closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the icons class
+       } else {
+         closeBtn.classList.replace("bx-menu-alt-right","bx-menu"); //replacing the icons class
        }
       }
 
